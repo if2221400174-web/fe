9bb2e4bdd272d4eff4be25ec46416c2b6d113988
Ubuntu <aril@aril.qktelinwehnu1gtz4qqpuwtaqg.ix.internal.cloudapp.net>
2026-07-20 15:18:10 +0000

    Update fe lagi4

--- src/pages/admin/pasien/index.jsx
+++ src/pages/admin/pasien/index.jsx
@@ -258,7 +258,7 @@ export default function AdminPasien() {
                         <svg className="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
                         </svg>
-                        {callingId === p.id ? "MENGIRIM..." : "KIRIM ANTRE"}
+                        {callingId === p.id ? "MENGIRIM..." : "KIRIM ANTREAN"}
                       </button>
                     </div>
 
